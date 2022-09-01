fc3a4a41429d939550d256832ade601972b3f1fd
Faine Mesquita <faine.mesquita@leapdev.io>
2022-09-01 11:14:40 +1000

    TextArea

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
 import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
+const SAMPLE_TEST="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 
 @Component({
   selector: "create-course-step-1",
@@ -20,7 +22,7 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEST, [Validators.required, Validators.minLength(3)]]
   });
 
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
